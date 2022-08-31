c92a97a95c4d1b5253b659dcc7f0dd9e062d5aa3
Damián Clancig <damian@clancig.com.ar>
2022-08-31 20:01:32 -0300

    Se agrega el footer.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,38 @@
+import facebookIcon from '../../img/facebook-icon.png'
+import instagramIcon from '../../img/instagram-icon.png'
+import './Footer.css'
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <section className='footer-container'>
+                <div className='social-section'>
+                    <h2>Redes sociales</h2>
+                    <ul className='listSocial'>
+                        <li>
+                            <img src={facebookIcon} alt='Facebook' width='30px' />
+                            <a href='https://www.facebook.com/ajalderaiz' target="blank">@ajalderaiz</a>
+                        </li>
+                        <li>
+                            <img src={instagramIcon} alt='Instagram' width='30px' />
+                            <a href='https://www.instagram.com/ajalderaiz/' target="blank">@ajalderaiz</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className='about-section'>
+                    <h2>Acerca de Ajal de Raiz</h2>
+                    <p>Somos un emprendimiento de plantas y suculentas, también ofrecemos sustratos, alimentos, y servicios para tus plantas.</p>
+                </div>
+                <div className='contact-section'>
+                    <h2>Contacto</h2>
+                    <li>
+                        Celular: <a href='https://api.whatsapp.com/send?phone=5491168793296' target="blank">+54 9 11 6879-3296</a>
+                    </li>
+                    <li>E-mail: <a href='mailto: contacto@ajalderaiz.com.ar' target="blank">contacto@ajalderaiz.com.ar</a></li>
+                    <li>Ubicación: Bernal, Buenos Aires.</li>
+                </div>
+            </section>
+            <section className='copyright'>Copyright &copy; Ajal de Raiz 2022</section>
+        </footer>
     )
 }
